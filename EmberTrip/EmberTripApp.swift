65f9c28e9d9bc2f058616486f00b86e2aad77108
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2023-12-13 18:01:21 +0000

    Initial Commit

---
+++ EmberTrip/EmberTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberTripApp.swift
+//  EmberTrip
+//
+//  Created by Elvis on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmberTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
